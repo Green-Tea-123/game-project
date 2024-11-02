fe0f41c47b9c73375eee2b0f541a41c933123b2e
shude1999 <zhoushude1@gmail.com>
2024-11-02 18:24:27 +0800

    3

--- game-project/Assets/Scripts/DialogueTrigger.cs
+++ game-project/Assets/Scripts/DialogueTrigger.cs
@@ -1,15 +1,13 @@
-using System.Collections;
 using System.Collections.Generic;
-using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
-
+ 
 [System.Serializable]
 public class DialogueCharacter
 {
     public string name;
     public Sprite icon;
 }
-
+ 
 [System.Serializable]
 public class DialogueLine
 {
@@ -17,29 +15,27 @@ public class DialogueLine
     [TextArea(3, 10)]
     public string line;
 }
-
+ 
 [System.Serializable]
 public class Dialogue
 {
     public List<DialogueLine> dialogueLines = new List<DialogueLine>();
 }
-
+ 
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
-
+ 
     public void TriggerDialogue()
     {
-        Dialoguemanager.instance.StartDialogue(dialogue);
+        DialogueManager.Instance.StartDialogue(dialogue);
     }
+ 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if(collision.tag == "Player")
         {
             TriggerDialogue();
         }
     }
 }
-
-
-
