834d35f38e54fb8a435ca624a40fa3f08ba1046e
Green-Tea-123 <junboonsoo@gmail.com>
2024-11-05 11:52:10 +0800

    Update MainManager to chek tasks done

--- game-project/Assets/Scripts/MainManager.cs
+++ game-project/Assets/Scripts/MainManager.cs
@@ -12,26 +12,27 @@ public class MainManager : MonoBehaviour {
     public static MainManager instance;
     private int dementiaCounter;
     private int day;
-    private List<string> tasks;
+    private HashSet<string> tasks;
+    private HashSet<string> tasksDone;
     // kitchen activites
-    private static readonly string BREAKFAST = "Make breakfast";
-    private static readonly string MEDICINE = "Take medicine";
+    public static readonly string BREAKFAST = "Make breakfast";
+    public static readonly string MEDICINE = "Take medicine";
 
     // road activites
-    private static readonly string TAICHI = "Attend Taichi session";
-    private static readonly string MAHJONG = "Play mahjong";
-    private static readonly string CLINIC = "Get medicine at the clinic";
+    public static readonly string TAICHI = "Attend Taichi session";
+    public static readonly string MAHJONG = "Play mahjong";
+    public static readonly string CLINIC = "Get medicine at the clinic";
 
     // groceries
-    private static readonly string GROCERIES_FISH = "Buy a fish";
-    private static readonly string GROCERIES_FRUIT = "Buy a banana";
-    private static readonly string GROCERIES_CABBAGE = "Buy a cabbage";
-    private static readonly string GROCERIES_POTATO = "Buy a potato";
-    private static readonly string GROCERIES_CARROT = "Buy a carrot";
-    private static readonly string GROCERIES_BEEF = "Buy beef";
-    private static readonly string GROCERIES_CHICKEN = "Buy a chicken";
-    private static readonly string GROCERIES_GINGER = "Buy some ginger";
-    private static readonly string GROCERIES_CARDAMOM = "Buy some cardamom";
+    public static readonly string GROCERIES_FISH = "Buy a fish";
+    public static readonly string GROCERIES_FRUIT = "Buy a banana";
+    public static readonly string GROCERIES_CABBAGE = "Buy a cabbage";
+    public static readonly string GROCERIES_POTATO = "Buy a potato";
+    public static readonly string GROCERIES_CARROT = "Buy a carrot";
+    public static readonly string GROCERIES_BEEF = "Buy beef";
+    public static readonly string GROCERIES_CHICKEN = "Buy a chicken";
+    public static readonly string GROCERIES_GINGER = "Buy some ginger";
+    public static readonly string GROCERIES_CARDAMOM = "Buy some cardamom";
 
     private void Awake() {
         if (instance != null) {
@@ -43,6 +44,7 @@ public class MainManager : MonoBehaviour {
         this.dementiaCounter = 0;
         this.day = 1;
         this.tasks = getDay1List();
+        this.tasksDone = new HashSet<string>();
         DontDestroyOnLoad(gameObject);
     }
 
@@ -66,8 +68,8 @@ public class MainManager : MonoBehaviour {
         this.day++;
     }
 
-    public static List<string> getDay1List() {
-        List<string> list = new List<string>();
+    public static HashSet<string> getDay1List() {
+        HashSet<string> list = new HashSet<string>();
         list.Add(BREAKFAST);
         list.Add(MEDICINE);
         list.Add(TAICHI);
@@ -77,16 +79,16 @@ public class MainManager : MonoBehaviour {
         return list;
     }
 
-    public static List<string> getDay2List() {
-        List<string> list = new List<string>();
+    public static HashSet<string> getDay2List() {
+        HashSet<string> list = new HashSet<string>();
         list.Add(MEDICINE);
         list.Add(MAHJONG);
         list.Add(CLINIC);
         return list;
     }
 
-    public static List<string> getDay3List() {
-        List<string> list = new List<string>();
+    public static HashSet<string> getDay3List() {
+        HashSet<string> list = new HashSet<string>();
         list.Add(BREAKFAST);
         list.Add(MEDICINE);
         list.Add(GROCERIES_FISH);
@@ -94,14 +96,25 @@ public class MainManager : MonoBehaviour {
         return list;
     }
 
-    public static List<string> getDay4List() {
-        List<string> list = new List<string>();
+    public static HashSet<string> getDay4List() {
+        HashSet<string> list = new HashSet<string>();
         list.Add(BREAKFAST);
         list.Add(MEDICINE);
         list.Add(GROCERIES_CARDAMOM);
         list.Add(GROCERIES_BEEF);
         list.Add(GROCERIES_CARROT);
         return list;
+    }
+
+    public void addTaskDone(string task) {
+        this.tasksDone.Add(task);
+    }
+
+    public void checkTasks() {
+        bool isSame = this.tasks.Equals(this.tasksDone);
+        if (!isSame) {
+            incrementDementiaCounter();
+        }
     }
 
     public void updateDayList() {
