539ae1b4ba76e3a1fa709542bc48269f17b733cd
shude1999 <zhoushude1@gmail.com>
2024-11-05 19:15:33 +0800

    Code xfer(ignore)

--- game-project/Assets/Scripts/DialogueTrigger.cs
+++ game-project/Assets/Scripts/DialogueTrigger.cs
@@ -1,41 +1,52 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
- 
-[System.Serializable]
-public class DialogueCharacter
-{
-    public string name;
-    public Sprite icon;
-}
- 
-[System.Serializable]
-public class DialogueLine
-{
-    public DialogueCharacter character;
-    [TextArea(3, 10)]
-    public string line;
-}
- 
-[System.Serializable]
-public class Dialogue
-{
-    public List<DialogueLine> dialogueLines = new List<DialogueLine>();
-}
- 
+
 public class DialogueTrigger : MonoBehaviour
 {
-    public Dialogue dialogue;
- 
-    public void TriggerDialogue()
+    [Header("Visual Cue")]
+    [SerializeField] private GameObject visualCue;
+
+    [Header("Ink JSON")]
+    [SerializeField] private TextAsset inkJSON;
+
+    private bool playerInRange;
+
+    private void Awake() 
     {
-        DialogueManager.Instance.StartDialogue(dialogue);
+        playerInRange = false;
+        visualCue.SetActive(false);
     }
- 
-    private void OnTriggerEnter2D(Collider2D collision)
+
+    private void Update() 
     {
-        if(collision.tag == "Player")
+        if (playerInRange && !DialogueManager.GetInstance().dialogueIsPlaying) 
         {
-            TriggerDialogue();
+            visualCue.SetActive(true);
+            if (InputManager.GetInstance().GetInteractPressed()) 
+            {
+                DialogueManager.GetInstance().EnterDialogueMode(inkJSON);
+            }
+        }
+        else 
+        {
+            visualCue.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) 
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider) 
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            playerInRange = false;
         }
     }
 }
--- game-project/Assets/Scripts/InputManager.cs
+++ game-project/Assets/Scripts/InputManager.cs
@@ -9,6 +9,56 @@ public class InputManager : MonoBehaviour
 
     private PlayerInput _playerinput;
     private InputAction _moveaction;
+    private bool interactPressed = false;
+
+    private bool submitPressed = false;
+
+    private static InputManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("Found more than one Input Manager in the scene.");
+        }
+        instance = this;
+    }
+
+    public static InputManager GetInstance() 
+    {
+        return instance;
+    }
+
+    public void InteractButtonPressed(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            interactPressed = true;
+        }
+        else if (context.canceled)
+        {
+            interactPressed = false;
+        } 
+    }
+
+    public void SubmitPressed(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            submitPressed = true;
+        }
+        else if (context.canceled)
+        {
+            submitPressed = false;
+        } 
+    }
+
+    public bool GetInteractPressed() 
+    {
+        bool result = interactPressed;
+        interactPressed = false;
+        return result;
+    }
 
     void Start()
     {
@@ -19,6 +69,18 @@ public class InputManager : MonoBehaviour
     void Update()
     {
        Movement =  _moveaction.ReadValue<Vector2>();
+    }
+
+     public bool GetSubmitPressed() 
+    {
+        bool result = submitPressed;
+        submitPressed = false;
+        return result;
+    }
+
+    public void RegisterSubmitPressed() 
+    {
+        submitPressed = false;
     }
 
 }
