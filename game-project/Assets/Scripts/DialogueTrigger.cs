7844ba26cb5a1d8b656f297ca66a49640e8078a4
shude1999 <zhoushude1@gmail.com>
2024-11-10 19:21:08 +0800

    Enable loading of 4 day dialogue into dialogue trigger

--- game-project/Assets/Scripts/DialogueTrigger.cs
+++ game-project/Assets/Scripts/DialogueTrigger.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour
 {
     [Header("Visual Cue")]
-    [SerializeField] private GameObject visualCue;
+    //[SerializeField] private GameObject visualCue;
 
     [Header("Ink JSON")]
-    [SerializeField] private TextAsset inkJSON;
+
+    [Header("Day 1 Ink")]
+    [SerializeField] private TextAsset day1Json;
+
+    [Header("Day 2 Ink")]
+    [SerializeField] private TextAsset day2Json;
+
+    [Header("Day 3 Ink")]
+    [SerializeField] private TextAsset day3Json;
+
+    [Header("Day 4 Ink")]
+    [SerializeField] private TextAsset day4Json;
 
     private bool playerInRange;
 
@@ -17,23 +28,48 @@ public class DialogueTrigger : MonoBehaviour
     private void Awake() 
     {
         playerInRange = false;
-        visualCue.SetActive(false);
+       // visualCue.SetActive(false);
     }
 
     private void Update() 
     {
+        int day = MainManager.instance.getDay();
         if (playerInRange && !DialogueManager.GetInstance().dialogueIsPlaying && !interacteditem) 
         {
-            visualCue.SetActive(true);
+            //visualCue.SetActive(true);
             if (InputManager.GetInstance().GetInteractPressed()) 
             {
                 interacteditem = true;
-                DialogueManager.GetInstance().EnterDialogueMode(inkJSON);
+                switch (day) {
+                    case 1:
+                        
+                        DialogueManager.GetInstance().EnterDialogueMode(day1Json);
+                        
+                        break;
+                    case 2:
+                        
+                        DialogueManager.GetInstance().EnterDialogueMode(day2Json);
+                        
+                        break;
+                    case 3:
+                        
+                        DialogueManager.GetInstance().EnterDialogueMode(day3Json);
+                        
+                        break;
+                    case 4:
+                        
+                        DialogueManager.GetInstance().EnterDialogueMode(day4Json);
+                        
+                        break;
+                    default:
+                        Debug.LogError("day " + day + " is out of bounds");
+                        break;
+                }
             }
         }
         else 
         {
-            visualCue.SetActive(false);
+            //visualCue.SetActive(false);
         }
     }
 
