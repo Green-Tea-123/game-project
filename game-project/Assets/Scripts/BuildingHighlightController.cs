6b45923cc6f6f0a85b531eaec8de64c0796ae157
shude1999 <zhoushude1@gmail.com>
2024-11-13 22:41:42 +0800

    Updated Building Tags

--- game-project/Assets/Scripts/BuildingHighlightController.cs
+++ game-project/Assets/Scripts/BuildingHighlightController.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 public class BuildingHighlightController : MonoBehaviour
 {
     public GameObject highlightOverlay; // Assign the yellow box prefab instance here in the Inspector
-
+    public GameObject textIntro;
     void Start()
     {
         // Ensure the overlay is hidden initially
         if (highlightOverlay != null)
         {
             highlightOverlay.SetActive(false);
+            textIntro.SetActive(false);
         }
     }
 
@@ -19,6 +20,7 @@ public class BuildingHighlightController : MonoBehaviour
         {
             // Show the yellow box overlay when the player enters the trigger
             highlightOverlay.SetActive(true);
+            textIntro.SetActive(true);
         }
     }
 
@@ -28,6 +30,7 @@ public class BuildingHighlightController : MonoBehaviour
         {
             // Hide the yellow box overlay when the player exits the trigger
             highlightOverlay.SetActive(false);
+            textIntro.SetActive(false);
         }
     }
 }
