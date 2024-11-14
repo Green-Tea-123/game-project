d4c9ee8b352d8860bd232fa5ae421c18aadac1cf
Zhou-Shude <138975672+shude1999@users.noreply.github.com>
2024-11-15 02:35:14 +0800

    linkage to goodEnding

--- game-project/Assets/Scripts/Change Scene Scripts/ChangeSceneBadEnding.cs
+++ game-project/Assets/Scripts/Change Scene Scripts/ChangeSceneBadEnding.cs
@@ -5,12 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class ChangeSceneBadEnding : ChangeSceneOnCollision {
     public string badSceneName;
+    public string goodSceneName;
     public int threshold;
 
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.CompareTag("Player")) {
-            if (MainManager.instance.getDay() == 2) { // MainManager.instance.getDementiaCounter() >= threshold
-                SceneManager.LoadScene(badSceneName);
+            if (MainManager.instance.getDay() == 4) { // MainManager.instance.getDementiaCounter() >= threshold
+                if (MainManager.instance.getDementiaCounter() >= threshold)
+                { 
+                    SceneManager.LoadScene(badSceneName); 
+                }
+                else
+                {
+                    SceneManager.LoadScene(goodSceneName);
+                }
             }
             else {
                 SceneManager.LoadScene(nextSceneName);
